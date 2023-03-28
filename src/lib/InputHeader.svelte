7a4449611229ad770ec12ad812af546beeec9931
Sören Maschmann <smaschmann@slashwhy.de>
2023-03-28 13:21:32 +0200

    refactor: full size city input field

--- src/lib/InputHeader.svelte
+++ src/lib/InputHeader.svelte
@@ -18,15 +18,15 @@
   };
 </script>
 
-<header class="flex h-16 items-center border-b-2 border-neutral-800 pl-2">
+<header class="flex h-16 items-center border-b-2 border-neutral-800">
   <form
-    class="flex h-full w-full items-center space-x-2 divide-x-2 divide-neutral-800"
+    class="flex h-full w-full items-center divide-x-2 divide-neutral-800"
     on:submit={onSubmit}
   >
-    <label class="relative grow" for={CITY_INPUT_NAME}>
+    <label class="relative h-full grow" for={CITY_INPUT_NAME}>
       <span class="sr-only block text-xs">City</span>
       <input
-        class="block w-full rounded-lg bg-slate-200 px-2 py-1 transition-colors placeholder:text-slate-400 hover:bg-slate-300 hover:placeholder:text-slate-100 focus:outline focus:outline-2 focus:outline-neutral-800 focus:hover:bg-slate-200 focus:hover:placeholder:text-slate-400"
+        class="block h-full w-full bg-slate-100 px-2 py-1 text-2xl transition-colors placeholder:text-neutral-800 hover:bg-slate-200 hover:placeholder:text-slate-400 focus:bg-slate-200 focus:outline-none"
         name={CITY_INPUT_NAME}
         type="text"
         placeholder="City..."
@@ -35,14 +35,14 @@
       />
     </label>
     <button
-      class="block h-full w-16 bg-amber-300 fill-neutral-800 p-3 transition-colors hover:bg-amber-400 focus:outline-none focus-visible:bg-amber-400 active:bg-amber-500"
+      class="block h-full w-16 flex-shrink-0 bg-amber-300 fill-neutral-800 p-3 transition-colors hover:bg-amber-400 focus:outline-none focus-visible:bg-amber-400 active:bg-amber-500"
       type="submit"
       aria-label="Search for locations"
     >
       <SearchIcon />
     </button>
     <button
-      class="!ml-0 block h-full w-16 bg-sky-300 fill-neutral-800 p-3 transition-colors hover:bg-sky-400 focus:outline-none focus-visible:bg-sky-400 active:bg-sky-500"
+      class="!ml-0 block h-full w-16 flex-shrink-0 bg-sky-300 fill-neutral-800 p-3 transition-colors hover:bg-sky-400 focus:outline-none focus-visible:bg-sky-400 active:bg-sky-500"
       type="button"
       aria-label="Get weather forecast for your location"
       on:click={() => dispatch('geolocation')}
