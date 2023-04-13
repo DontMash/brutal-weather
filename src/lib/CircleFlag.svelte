209e60adcb492e931f2b323746aaadd9fdf2dece
Sören Maschmann <smaschmann@slashwhy.de>
2023-04-13 12:00:21 +0200

    style: add border to circle-flag component;

--- src/lib/CircleFlag.svelte
+++ src/lib/CircleFlag.svelte
@@ -3,10 +3,12 @@
   export let country_code: string;
 </script>
 
-<img
-  class="w-8"
-  src={`https://hatscripts.github.io/circle-flags/flags/${country_code.toLowerCase()}.svg`}
-  width="48"
-  height="48"
-  alt={country}
-/>
+<picture class="overflow-hidden rounded-full border-2 border-neutral-800">
+  <img
+    class="w-8 scale-[1.02]"
+    src={`https://hatscripts.github.io/circle-flags/flags/${country_code.toLowerCase()}.svg`}
+    width="48"
+    height="48"
+    alt={country}
+  />
+</picture>
